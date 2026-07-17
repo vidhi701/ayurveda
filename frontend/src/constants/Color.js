5ace2db1e4eae7ef3b49c772d178ac7e9dafb969
vidhi701 <gvidhi701@gmail.com>
2026-07-17 16:57:17 +0530

    added

---
+++ frontend/src/constants/Color.js
@@ -0,0 +1,17 @@
+export const COLORS = {
+
+    background:"#F8F5E8",
+
+    surface:"#FFFDF6",
+
+    primary:"#9A6A43",
+
+    primaryDark:"#6D4C2F",
+
+    accent:"#C8A26A",
+
+    border:"#E9DFC9",
+
+    text:"#6F6258",
+
+}

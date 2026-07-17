ff8dee35f8637005309a38a3957412d41ceafbeb
vidhi701 <gvidhi701@gmail.com>
2026-07-17 16:34:07 +0530

    added successfully

---
+++ ayurveda_ecommerce/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
+import HeaderIcons from "./Headericon";
+import TopBar from "./TopBar";
+
+const Header = () => {
+  return (
+    <>
+    <TopBar />
+    <header
+      className="sticky top-0 z-50
+      w-full
+      bg-[#FAF8F2]
+      border-b border-[#E8DDC9]
+      shadow-sm"
+    >
+      <div
+        className="max-w-[1400px]
+        mx-auto
+        flex
+        items-center
+        justify-between
+        px-8
+        py-5"
+      >
+        <Logo />
+
+        <NavLinks />
+
+        <HeaderIcons />
+      </div>
+    </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ ayurveda_ecommerce/frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import "./index.css";
+
+// Fonts
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
